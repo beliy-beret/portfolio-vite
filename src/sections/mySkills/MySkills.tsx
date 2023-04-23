4a5b7fe1ed7efeed03c818e474f43541cbecf281
AlucarD <skoriy12990@gmail.com>
2023-04-23 17:13:17 +0300

    add Fade animation to skills section

--- src/sections/mySkills/MySkills.tsx
+++ src/sections/mySkills/MySkills.tsx
@@ -1,3 +1,4 @@
+import { Fade } from 'react-awesome-reveal'
 import { Section } from '../../components/mySection/MySection'
 import { Title } from '../../components/title/Title'
 import classes from './mySkills.module.css'
@@ -7,18 +8,20 @@ export const MySkills = () => {
   return (
     <Section sectionName='skills' id='skills' className={classes.skills}>
       <Title>Work with:</Title>
-      <div className={classes.imgList}>
-        <img src={skillsImages.htmlIcon} alt={'HTML'} />
-        <img src={skillsImages.cssIcon} alt={'CSS / SCSS'} />
-        <img src={skillsImages.jsIcon} alt={'JavaScript'} />
-        <img src={skillsImages.tsIcon} alt={'TypeScript'} />
-        <img src={skillsImages.reactIcon} alt={'React'} />
-        <img src={skillsImages.reduxIcon} alt={'Redux'} />
-        <img src={skillsImages.jestIcon} alt={'Jest'} />
-        <img src={skillsImages.storybookIcon} alt={'Storybook'} />
-        <img src={skillsImages.webpackIcon} alt={'Webpack'} />
-        <img src={skillsImages.dockerIcon} alt={'docker'} />
-      </div>
+      <Fade delay={400}>
+        <div className={classes.imgList}>
+          <img src={skillsImages.htmlIcon} alt={'HTML'} />
+          <img src={skillsImages.cssIcon} alt={'CSS / SCSS'} />
+          <img src={skillsImages.jsIcon} alt={'JavaScript'} />
+          <img src={skillsImages.tsIcon} alt={'TypeScript'} />
+          <img src={skillsImages.reactIcon} alt={'React'} />
+          <img src={skillsImages.reduxIcon} alt={'Redux'} />
+          <img src={skillsImages.jestIcon} alt={'Jest'} />
+          <img src={skillsImages.storybookIcon} alt={'Storybook'} />
+          <img src={skillsImages.webpackIcon} alt={'Webpack'} />
+          <img src={skillsImages.dockerIcon} alt={'docker'} />
+        </div>
+      </Fade>
     </Section>
   )
 }
