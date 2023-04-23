04c128a2b87b301fbd2c5278d78f5901286cbbd6
AlucarD <skoriy12990@gmail.com>
2023-04-23 15:55:50 +0300

    add scroll animation

--- src/sections/header/nav/NavMenu.tsx
+++ src/sections/header/nav/NavMenu.tsx
@@ -1,21 +1,39 @@
 import { FC } from 'react'
 import classes from './navMenu.module.css'
+import { scroller } from 'react-scroll'
 import { useAppContext } from '../../../App'
 
 export const NavMenu: FC = () => {
   const { activeSectionName } = useAppContext()
+  const scrollOptions = {
+    duration: 700,
+    delay: 0,
+    smooth: 'easeInOutQuart',
+  }
+  const scrollTo = {
+    about: () => scroller.scrollTo('about', scrollOptions),
+    skills: () => scroller.scrollTo('skills', scrollOptions),
+    projects: () => scroller.scrollTo('projects', scrollOptions),
+    contacts: () => scroller.scrollTo('contacts', scrollOptions),
+  }
   return (
     <nav className={classes.nav}>
-      <a href='#about' className={activeSectionName === 'about' ? classes.active : ''}>
+      <a className={activeSectionName === 'about' ? classes.active : ''} onClick={scrollTo.about}>
         About
       </a>
-      <a href='#skills' className={activeSectionName === 'skills' ? classes.active : ''}>
+      <a className={activeSectionName === 'skills' ? classes.active : ''} onClick={scrollTo.skills}>
         Skills
       </a>
-      <a href='#projects' className={activeSectionName === 'projects' ? classes.active : ''}>
+      <a
+        className={activeSectionName === 'projects' ? classes.active : ''}
+        onClick={scrollTo.projects}
+      >
         Projects
       </a>
-      <a href='#contacts' className={activeSectionName === 'contacts' ? classes.active : ''}>
+      <a
+        className={activeSectionName === 'contacts' ? classes.active : ''}
+        onClick={scrollTo.contacts}
+      >
         Contacts
       </a>
     </nav>
