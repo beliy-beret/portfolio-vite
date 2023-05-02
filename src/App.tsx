415a63542d68647b7dfe2069cf884e9f90dd8808
AlucarD <skoriy12990@gmail.com>
2023-05-02 15:35:51 +0300

    add spiner add initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { createContext, useContext, useState } from 'react'
+import { ConfigProvider, Spin } from 'antd'
+import { createContext, useContext, useEffect, useState } from 'react'
 
 import { About } from './sections/about/About'
-import { ConfigProvider } from 'antd'
 import { Contacts } from './sections/myContacts/Contacts'
 import { Container } from './components/container/Container'
 import { Footer } from './sections/footer/Footer'
@@ -18,9 +18,22 @@ export const AppContext = createContext<ContextType>({} as ContextType)
 export const useAppContext = () => useContext<ContextType>(AppContext)
 
 export const App = () => {
+  const [isInit, setIsInit] = useState(false)
   const [activeSectionName, setActiveSectionName] = useState<SectionNamesType>(
     '' as SectionNamesType,
   )
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setIsInit(true), 800)
+    return () => clearTimeout(timeoutId)
+  }, [])
+
+  if (!isInit) {
+    return (
+      <div className='spiner'>
+        <Spin tip='Loading' size='large' />
+      </div>
+    )
+  }
   return (
     <AppContext.Provider value={{ activeSectionName, setActiveSectionName }}>
       <ConfigProvider
@@ -30,18 +43,16 @@ export const App = () => {
           },
         }}
       >
-        <div className={'app'}>
-          <Header />
-          <main>
-            <Container>
-              <About />
-              <MySkills />
-              <Projects />
-              <Contacts />
-            </Container>
-          </main>
-          <Footer />
-        </div>
+        <Header />
+        <main>
+          <Container>
+            <About />
+            <MySkills />
+            <Projects />
+            <Contacts />
+          </Container>
+        </main>
+        <Footer />
       </ConfigProvider>
     </AppContext.Provider>
   )
